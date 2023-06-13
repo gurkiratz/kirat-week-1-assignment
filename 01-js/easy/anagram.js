5f0efca380ce68d348391d0c992db4c446f3a54e
Gurkirat Singh <singh.gurkirat2707@gmail.com>
2023-06-13 18:10:43 +0530

    Done 01-js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,17 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
+const mySort = (str) => {
+  return str.toLowerCase().replace(/\s/g, '').split('').sort().join('');
+};
 
+function isAnagram(str1, str2) {
+  const sortedStr1 = mySort(str1);
+  const sortedStr2 = mySort(str2);
+
+  console.log(sortedStr1, sortedStr2);
+
+  return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
